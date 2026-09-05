2231ac778a865bcb087b280dcddd8081108cdc0b
NachoParedes <nachoparedes82@gmail.com>
2026-09-05 17:40:57 -0300

    feat(sql): actualizar MER, modelo relacional y tabla jugador con metricas defensivas y expectativas

--- sql/02_Create_Tables.sql
+++ sql/02_Create_Tables.sql
@@ -21,14 +21,27 @@ CREATE TABLE IF NOT EXISTS jugador (
     nombre VARCHAR(50) NOT NULL,
     apellido VARCHAR(50) NOT NULL,
     minutos_jugados INT DEFAULT 0,
+    
+    -- Rendimiento Ofensivo
     goles INT DEFAULT 0,
     asistencias INT DEFAULT 0,
     goles_esperados DECIMAL(5,2) DEFAULT 0.00,
     asistencias_esperadas DECIMAL(5,2) DEFAULT 0.00,
-    vallas_invictas INT DEFAULT 0,
+    
+    -- Rendimiento Defensivo
+    recuperaciones INT DEFAULT 0,
+    entradas INT DEFAULT 0,
+    despejes_bloqueos_intercepciones INT DEFAULT 0,
+
+    
+    -- Arquero y Disciplina
     atajadas INT DEFAULT 0,
+    vallas_invictas INT DEFAULT 0,
+    goles_recibidos_esperados DECIMAL(5,2) DEFAULT 0.00,
     tarjetas_rojas INT DEFAULT 0,
     tarjetas_amarillas INT DEFAULT 0,
+    
+    -- Claves Foráneas
     id_equipo INT NOT NULL,
     id_posicion INT NOT NULL,
     
