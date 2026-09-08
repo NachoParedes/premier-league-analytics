1213f9eb3f944b3a18a89103d4251a4080f041e4
NachoParedes <nachoparedes82@gmail.com>
2026-09-08 17:17:13 -0300

    feat(transform): agregar modulo de transformacion de datos con pandas

---
+++ src/transform.py
@@ -0,0 +1,90 @@
+import pandas as pd
+
+
+def transformar_equipos(datos_raw):
+    if not datos_raw or "teams" not in datos_raw:
+        return pd.DataFrame()
+
+    df = pd.DataFrame(datos_raw["teams"])
+
+    columnas_mapeo = {
+        "id": "id_equipo",
+        "code": "codigo",
+        "name": "nombre",
+        "short_name": "abreviatura",
+    }
+
+    df_equipos = df[list(columnas_mapeo.keys())].rename(columns=columnas_mapeo)
+    return df_equipos.astype({"id_equipo": int, "codigo": int})
+
+
+def transformar_juegadores(datos_raw):
+    if "elements" not in datos_raw:
+        return pd.DataFrame()
+
+    df = pd.DataFrame(datos_raw["elements"])
+
+    columnas_mapeo = {
+        "id": "id_jugador",
+        "first_name": "nombre",
+        "second_name": "apellido",
+        "minutes": "minutos_jugados",
+
+        # Rendimiento ofensivo
+        "goals_scored": "goles",
+        "assists": "asistencias",
+        "expected_goals": "goles_esperados",
+        "expected_assists": "asistencias_esperadas",
+
+        # Rendimiento defensivo
+        "recoveries": "recuperaciones",
+        "tackles": "entradas",
+        "clearances_blocks_interceptions": "despejes_bloqueos_intercepciones",
+        "clean_sheets": "vallas_invictas",
+        "expected_goals_conceded": "goles_recibidos_esperados",
+
+        # Arquero y Disciplina
+        "saves": "atajadas",
+        "red_cards": "tarjetas_rojas",
+        "yellow_cards": "tarjetas_amarillas",
+
+        # Claves foráneas
+        "team": "id_equipo",
+        "element_type": "id_posicion",
+    }
+
+
+    df = df[list(columnas_mapeo.keys())].rename(columns=columnas_mapeo)
+    return df.astype({"id_jugador": int})
+
+
+def transformar_posiciones(datos_raw):
+
+    if not datos_raw or "element_types" not in datos_raw:
+        return pd.DataFrame()
+
+    df = pd.DataFrame(datos_raw["element_types"])
+    
+    columnas_mapeo = {
+        "id": "id_posicion",
+        "singular_name": "nombre_posicion",
+        "singular_name_short": "abreviatura"
+    }
+
+    return df[list(columnas_mapeo.keys())].rename(columns=columnas_mapeo)
+
+
+if __name__ == "__main__":
+
+    from extract import obtener_datos
+
+    raw_data = obtener_datos()
+    if raw_data:
+        df_equipo = transformar_equipos(raw_data)
+        df_jugador = transformar_juegadores(raw_data)
+        df_posiciones = transformar_posiciones(raw_data)
+        print(df_equipo.head())
+        print("--------------------------------------------------------")
+        print(df_jugador.head())
+        print("--------------------------------------------------------")
+        print(df_posiciones.head())
